d34b5ee95f76a6022b3435ee905d0b64dc20db6f
Creewie <tabero2006@gmail.com>
2024-11-17 17:24:59 +0100

    Dodawanie / Odejmowanie / Edytowanie

--- ContactManager/MainWindow.axaml.cs
+++ ContactManager/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ContactManager;
 
 namespace ContactMenager;
 
@@ -10,6 +13,7 @@ public class Contact {
     public string Number { get; set; }
     public string Email { get; set; }
     public bool IsFavourite { get; set; }
+    public ICommand DeleteCommand { get; set; }
 }
 
 public partial class MainWindow : Window
@@ -17,13 +21,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        _kontakty = new(){
-            new Contact(){Name = "Krzysiu", Surname = "Mayanowski", Number = "420 549 911", Email = "autobrumbrum@gmail.com", IsFavourite = true},
-            new Contact(){Name = "Bartłomiej", Surname = "Nemendricki", Number = "420 420 420", Email = "cpunendricki@gmail.com", IsFavourite = true},
-            new Contact(){Name = "Stefan", Surname = "Leoński", Number = "600 100 100", Email = "bocian.porzyczki@gmail.com", IsFavourite = false},
-            new Contact(){Name = "Phelipe", Surname = "Mountan", Number = "420 549 911", Email = "gorol@gmail.com", IsFavourite = false},
-            new Contact(){Name = "Dominik", Surname = "Winiarz", Number = "0 1975 1995", Email = "partyjniakwiniarz@gmail.com", IsFavourite = true},
-        };
+
+        AddContact("Krzysiu", "Mayanowski", "420 549 911", "autobrumbrum@gmail.com", true);
+        AddContact("Bartłomiej", "Nemendricki", "420 420 420", "cpunendricki@gmail.com", true);
+        AddContact("Stefan", "Leoński", "600 100 100", "bocian.porzyczki@gmail.com");
+        AddContact("Phelipe", "Mountan", "420 549 911", "gorol@gmail.com");
+        AddContact("Dominik", "Winiarz", "0 1975 1995", "partyjniakwiniarz@gmail.com", true);
+
         Kontakty.ItemsSource = filteredContacts;
         Category.ItemsSource = new string[]{"Wszystkie", "Ulubione"};
         Category.SelectedIndex = 0;
@@ -46,6 +50,22 @@ public partial class MainWindow : Window
         }
     }
 
+    private void AddContact(string name, string surname, string number, string email, bool isFavourite = false) {
+        var contact = new Contact() {
+            Name = name,
+            Surname = surname,
+            Number = number,
+            Email = email,
+            IsFavourite = isFavourite
+        };
+        contact.DeleteCommand = new Command(() => {
+            _kontakty.Remove(contact);
+            Filter();
+        });
+        _kontakty.Add(contact);
+        Filter();
+    }
+
     private void CategoryChanged(object? sender, SelectionChangedEventArgs e)
     {
         Filter();
@@ -55,4 +75,15 @@ public partial class MainWindow : Window
     {
         Filter();
     }
+
+    private void AddContact(object? sender, RoutedEventArgs e)
+    {
+        AddContact(Name.Text, Surname.Text, Number.Text, Email.Text);
+        Name.Text = string.Empty;
+        Surname.Text = string.Empty;
+        Number.Text = string.Empty;
+        Email.Text = string.Empty;
+        
+    }
+    
 }
