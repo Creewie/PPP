e4bdf108d93988c27c67dbca4e76891d9fd77152
Creewie <tabero2006@gmail.com>
2024-11-17 18:25:45 +0100

    Ide grać w csa

--- EventCalendar/MainWindow.axaml.cs
+++ EventCalendar/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace EventCalendar;
 
@@ -20,8 +22,17 @@ public partial class MainWindow : Window
             new Event { Name = "Początek tej katastrofy :(", Day = "Poniedziałek", IsImportant = false },
             new Event { Name = "Urodziny ziomeczka! (Chlanie)", Day = "Piątek", IsImportant = true },
         };
+        Day.ItemsSource = new []{"Poniedziałek","Wtorek","Środa","Czwartek","Piątek","Sobota","Niedziela"};
         Events.ItemsSource = _events;
     }
 
     private readonly ObservableCollection<Event> _events = [];
+
+    private void AddEvent(object? sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Day.SelectedItem as string)) {
+            return;
+        }
+        _events.Add(new Event(){Name = Name.Text ?? "Puste", Day = (Day.SelectedItem as string ?? String.Empty), IsImportant = false});
+    }
 }
